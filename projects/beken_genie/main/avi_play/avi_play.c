4b3a3a7c7e7a6bf7da3ae8cc01b2611f44316bc7
itester <itester@aclsemi.com>
2025-03-15 17:39:24 +0800

    [CI] [54105]  <multimedia> <perf> add interface for switching avi files

--- projects/beken_genie/main/avi_play/avi_play.c
+++ projects/beken_genie/main/avi_play/avi_play.c
@@ -30,19 +30,31 @@ const lcd_open_t lcd_open =
 #if (CONFIG_SYS_CPU1)
 #include "lv_jpeg_hw_decode.h"
 
-static avi_t *avi = NULL;
-static uint32_t *video_frame = NULL;
-static uint32_t video_num = 0;
-static uint32_t video_len = 0;
-static uint32_t pos = 0;
-static uint16_t *framebuffer = NULL;
-static uint16_t *segmentbuffer = NULL;
-static uint32_t frame_size = 0;
-static jd_output_format *format = NULL;
+#define AVI_VIDEO_USE_HW_DECODE    1
+#define AVI_VIDEO_MAX_FRAME_LEN    (30 * 1024)
+
+
+typedef struct
+{
+    avi_t *avi;
+    uint32_t *video_frame;
+    uint32_t video_len;
+    uint32_t video_num;
+
+    uint16_t *framebuffer;
+    uint16_t *segmentbuffer;
+
+    uint32_t frame_size;
+    uint8_t video_segment_flag;
+    uint32_t pos;
+} bk_avi_play_t;
+
+
+static bk_avi_play_t bk_avi_play = {0};
 static lv_vnd_config_t lv_vnd_config = {0};
 
 static lv_obj_t *img = NULL;
-static lv_timer_t *timer;
+static lv_timer_t *timer = NULL;
 static lv_img_dsc_t img_dsc =
 {
     .header.cf = LV_IMG_CF_TRUE_COLOR,
@@ -53,95 +65,196 @@ static lv_img_dsc_t img_dsc =
     .data = NULL,
 };
 
-#define AVI_VIDEO_USE_HW_DECODE    1
-
-static void avi_video_frame_parse_to_rgb565(avi_t *AVI, uint32_t frame, uint8_t *src_buf, uint8_t *dst_buf, uint32_t src_size, uint32_t outbuf_size)
+static void bk_avi_play_config_free(bk_avi_play_t *avi_play)
 {
-    AVI_set_video_position(AVI, frame, (long *)&src_size);
-    AVI_read_frame(AVI, (char *)src_buf, src_size);
-
-    if (src_size == 0)
+    if (avi_play->avi)
     {
-        frame = frame + 1;
-        AVI_set_video_position(AVI, frame, (long *)&src_size);
-        AVI_read_frame(AVI, (char *)src_buf, src_size);
+        AVI_close(avi_play->avi);
+        avi_play->avi = NULL;
+    }
+
+    if (avi_play->video_frame)
+    {
+        psram_free(avi_play->video_frame);
+        avi_play->video_frame = NULL;
+    }
+
+    if (avi_play->framebuffer)
+    {
+        psram_free(avi_play->framebuffer);
+        avi_play->framebuffer = NULL;
+    }
+
+    if (avi_play->segmentbuffer)
+    {
+        psram_free(avi_play->segmentbuffer);
+        avi_play->segmentbuffer = NULL;
+    }
+
+    avi_play->pos = 0;
+}
+
+static bk_err_t bk_avi_play_open(bk_avi_play_t *avi_play, const char *filename, uint8_t segment_flag)
+{
+    LOGI("%s [%d]\r\n", __func__, __LINE__);
+
+    os_memset(avi_play, 0x00, sizeof(bk_avi_play_t));
+
+    avi_play->avi = AVI_open_input_file(filename, 1);
+    if (avi_play->avi == NULL)
+    {
+        LOGE("%s open avi file failed\r\n", __func__);
+        return BK_FAIL;
+    }
+    else
+    {
+        avi_play->video_num = AVI_video_frames(avi_play->avi);
+        avi_play->frame_size = avi_play->avi->width * avi_play->avi->height * 2;
+        LOGI("avi video_num: %d, width: %d, height: %d, frame_size: %d\r\n", avi_play->video_num, avi_play->avi->width, avi_play->avi->height, avi_play->frame_size);
+    }
+
+    avi_play->pos = 0;
+    avi_play->video_segment_flag = segment_flag;
+
+    avi_play->video_frame = psram_malloc(AVI_VIDEO_MAX_FRAME_LEN);
+    if (avi_play->video_frame == NULL)
+    {
+        LOGE("%s video_frame malloc fail\r\n", __func__);
+        AVI_close(avi_play->avi);
+        return BK_FAIL;
+    }
+
+    avi_play->framebuffer = psram_malloc(avi_play->frame_size);
+    if (avi_play->framebuffer == NULL)
+    {
+        LOGE("%s framebuffer malloc fail\r\n", __func__);
+        goto out;
+    }
+
+    if (avi_play->video_segment_flag == 1)
+    {
+        avi_play->segmentbuffer = psram_malloc(avi_play->frame_size);
+        if (avi_play->segmentbuffer == NULL)
+        {
+            LOGE("%s %d segmentbuffer malloc fail\r\n", __func__, __LINE__);
+            goto out;
+        }
     }
 
 #if AVI_VIDEO_USE_HW_DECODE
-    bk_jpeg_hw_decode_to_mem(src_buf, dst_buf, src_size, avi->width, avi->height);
-
-    uint16_t *buf16 = (uint16_t *)dst_buf;
-    for (int k = 0; k < 320 * 160; k++)
-    {
-        buf16[k] = ((buf16[k] & 0xff00) >> 8) | ((buf16[k] & 0x00ff) << 8);
-    }
+    bk_jpeg_hw_decode_to_mem_init();
 #else
-    sw_jpeg_dec_res_t result;
-    bk_jpeg_dec_sw_start(JPEGDEC_BY_FRAME, src_buf, dst_buf, src_size, outbuf_size, (sw_jpeg_dec_res_t *)&result);
+    jd_output_format format = {0};
+
+    bk_jpeg_dec_sw_init(NULL, 0);
+    format.format = JD_FORMAT_RGB565;
+    format.scale = 0;
+    format.byte_order = JD_BIG_ENDIAN;
+    jd_set_output_format(format);
 #endif
+    LOGI("%s complete\r\n", __func__);
+
+    return BK_OK;
+out:
+    bk_avi_play_config_free(avi_play);
+
+    return BK_FAIL;
 }
 
-static void lv_timer_cb(lv_timer_t *timer)
+static void bk_avi_play_close(bk_avi_play_t *avi_play)
 {
-    pos++;
-    if (pos >= video_num)
-    {
-        pos = 0;
-    }
-
-    avi_video_frame_parse_to_rgb565(avi, pos, (uint8_t *)video_frame, (uint8_t *)framebuffer, video_len, frame_size);
-
-    for (int i = 0; i < avi->height; i++)
-    {
-        os_memcpy(segmentbuffer + i * (avi->width >> 1), framebuffer + i * avi->width, avi->width);
-        os_memcpy(segmentbuffer + (avi->width >> 1) * avi->height + i * (avi->width >> 1), framebuffer + i * avi->width + (avi->width >> 1), avi->width);
-    }
-
-    lv_img_set_src(img, &img_dsc);
-}
-
-bk_err_t lvgl_event_close_handle(media_mailbox_msg_t *msg)
-{
-    lv_vendor_disp_lock();
-    lv_timer_del(timer);
-    lv_obj_del(img);
-    lv_vendor_disp_unlock();
-
-    lv_vendor_stop();
-    lcd_display_close();
-    AVI_close(avi);
-
 #if AVI_VIDEO_USE_HW_DECODE
     bk_jpeg_hw_decode_to_mem_deinit();
 #else
     bk_jpeg_dec_sw_deinit();
 #endif
 
-    if (video_frame)
+    bk_avi_play_config_free(avi_play);
+
+    LOGI("%s complete\r\n", __func__);
+}
+
+static void bk_avi_video_prase_to_rgb565(bk_avi_play_t *avi_play)
+{
+    AVI_set_video_position(avi_play->avi, avi_play->pos, (long *)&avi_play->video_len);
+    AVI_read_frame(avi_play->avi, (char *)avi_play->video_frame, avi_play->video_len);
+
+    if (avi_play->video_len == 0)
     {
-        os_free(video_frame);
-        video_frame = NULL;
+        avi_play->pos = avi_play->pos + 1;
+        AVI_set_video_position(avi_play->avi, avi_play->pos, (long *)&avi_play->video_len);
+        AVI_read_frame(avi_play->avi, (char *)avi_play->video_frame, avi_play->video_len);
     }
 
-    if (framebuffer)
+#if AVI_VIDEO_USE_HW_DECODE
+    bk_jpeg_hw_decode_to_mem((uint8_t *)avi_play->video_frame, (uint8_t *)avi_play->framebuffer, avi_play->video_len, avi_play->avi->width, avi_play->avi->height);
+
+    uint16_t *buf16 = (uint16_t *)avi_play->framebuffer;
+    for (int k = 0; k < 320 * 160; k++)
     {
-        os_free(framebuffer);
-        framebuffer = NULL;
+        buf16[k] = ((buf16[k] & 0xff00) >> 8) | ((buf16[k] & 0x00ff) << 8);
+    }
+#else
+    sw_jpeg_dec_res_t result;
+    bk_jpeg_dec_sw_start(JPEGDEC_BY_FRAME, (uint8_t *)avi_play->video_frame, (uint8_t *)avi_play->framebuffer, avi_play->video_len, avi_play->frame_size, (sw_jpeg_dec_res_t *)&result);
+#endif
+
+    if (avi_play->video_segment_flag == 1)
+    {
+        for (int i = 0; i < avi_play->avi->height; i++)
+        {
+            os_memcpy(avi_play->segmentbuffer + i * (avi_play->avi->width >> 1), avi_play->framebuffer + i * avi_play->avi->width, avi_play->avi->width);
+            os_memcpy(avi_play->segmentbuffer + (avi_play->avi->width >> 1) * avi_play->avi->height + i * (avi_play->avi->width >> 1), avi_play->framebuffer + i * avi_play->avi->width + (avi_play->avi->width >> 1), avi_play->avi->width);
+        }
+    }
+}
+
+static void lv_timer_cb(lv_timer_t *timer)
+{
+    bk_avi_play.pos++;
+    if (bk_avi_play.pos >= bk_avi_play.video_num)
+    {
+        bk_avi_play.pos = 0;
     }
 
-    if (segmentbuffer)
-    {
-        os_free(segmentbuffer);
-        segmentbuffer = NULL;
-    }
+    bk_avi_video_prase_to_rgb565(&bk_avi_play);
 
-    if (format)
-    {
-        os_free(format);
-        format = NULL;
-    }
+    lv_img_set_src(img, &img_dsc);
+}
 
-    pos = 0;
+static void bk_avi_play_start(void)
+{
+    lv_vendor_disp_lock();
+    img = lv_img_create(lv_scr_act());
+    lv_img_set_src(img, &img_dsc);
+    lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
+
+    timer = lv_timer_create(lv_timer_cb, 1000 / bk_avi_play.avi->fps, NULL);
+    lv_vendor_disp_unlock();
+
+    LOGI("%s complete\r\n", __func__);
+}
+
+static void bk_avi_play_stop(void)
+{
+    lv_vendor_disp_lock();
+    lv_timer_del(timer);
+    lv_obj_del(img);
+    lv_vendor_disp_unlock();
+
+    LOGI("%s complete\r\n", __func__);
+}
+
+bk_err_t lvgl_event_close_handle(media_mailbox_msg_t *msg)
+{
+    LOGI("%s \r\n", __func__);
+
+    bk_avi_play_stop();
+
+    lv_vendor_stop();
+    lcd_display_close();
+
+    bk_avi_play_close(&bk_avi_play);
 
     return BK_OK;
 }
@@ -151,7 +264,7 @@ bk_err_t lvgl_event_open_handle(media_mailbox_msg_t *msg)
 {
     bk_err_t ret = BK_FAIL;
 
-    LOGI("%s EVENT_LVGL_OPEN_IND \n", __func__);
+    LOGI("%s \n", __func__);
 
     lcd_open_t *lcd_open = (lcd_open_t *)msg->param;
 
@@ -191,90 +304,26 @@ bk_err_t lvgl_event_open_handle(media_mailbox_msg_t *msg)
     img_dsc.header.h = lv_vnd_config.lcd_ver_res;
     img_dsc.data_size = img_dsc.header.w * img_dsc.header.h * 2;
 
-    avi = AVI_open_input_file("/genie_eye.avi", 1);
-    if (avi != NULL)
+    ret = bk_avi_play_open(&bk_avi_play, "/genie_eye.avi", 1);
+    if (ret != BK_OK)
     {
-        video_num = AVI_video_frames(avi);
-        frame_size = avi->width * avi->height * 2;
-        LOGI("avi video_num: %d, width: %d, height: %d, frame_size: %d\r\n", video_num, avi->width, avi->height, frame_size);
+        LOGE("%s bk_avi_play_open failed\r\n", __func__);
+        lcd_display_close();
+        return ret;
+    }
+
+    if (bk_avi_play.video_segment_flag == 1)
+    {
+        img_dsc.data = (const uint8_t *)bk_avi_play.segmentbuffer;
     }
     else
     {
-        LOGE("open avi fail\r\n");
-        lcd_display_close();
-        return ret;
+        img_dsc.data = (const uint8_t *)bk_avi_play.framebuffer;
     }
 
-    video_frame = psram_malloc(30 * 1024);
-    if (video_frame == NULL)
-    {
-        LOGE("%s %d video_frame malloc fail\r\n", __func__, __LINE__);
-        AVI_close(avi);
-        lcd_display_close();
-        return ret;
-    }
+    bk_avi_video_prase_to_rgb565(&bk_avi_play);
 
-    framebuffer = psram_malloc(frame_size);
-    if (framebuffer == NULL)
-    {
-        LOGE("%s %d framebuffer malloc fail\r\n", __func__, __LINE__);
-        AVI_close(avi);
-        os_free(video_frame);
-        lcd_display_close();
-        return ret;
-    }
-
-    segmentbuffer = psram_malloc(frame_size);
-    if (segmentbuffer == NULL)
-    {
-        LOGE("%s %d framebuffer malloc fail\r\n", __func__, __LINE__);
-        AVI_close(avi);
-        os_free(video_frame);
-        os_free(framebuffer);
-        lcd_display_close();
-        return ret;
-    }
-
-    format = os_malloc(sizeof(jd_output_format));
-    if (format == NULL)
-    {
-        LOGE("%s %d format malloc fail\r\n", __func__, __LINE__);
-        AVI_close(avi);
-        os_free(video_frame);
-        os_free(framebuffer);
-        os_free(segmentbuffer);
-        lcd_display_close();
-        return ret;
-    }
-
-#if AVI_VIDEO_USE_HW_DECODE
-    bk_jpeg_hw_decode_to_mem_init();
-#else
-    bk_jpeg_dec_sw_init(NULL, 0);
-    format->format = JD_FORMAT_RGB565;
-    format->scale = 0;
-    format->byte_order = JD_BIG_ENDIAN;
-    jd_set_output_format(format);
-#endif
-
-    avi_video_frame_parse_to_rgb565(avi, pos, (uint8_t *)video_frame, (uint8_t *)framebuffer, video_len, frame_size);
-
-    for (int i = 0; i < avi->height; i++)
-    {
-        os_memcpy(segmentbuffer + i * (avi->width >> 1), framebuffer + i * avi->width, avi->width);
-        os_memcpy(segmentbuffer + (avi->width >> 1) * avi->height + i * (avi->width >> 1), framebuffer + i * avi->width + (avi->width >> 1), avi->width);
-    }
-
-    img_dsc.data = (const uint8_t *)segmentbuffer;
-
-    lv_vendor_disp_lock();
-    img = lv_img_create(lv_scr_act());
-    lv_img_set_src(img, &img_dsc);
-    lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
-
-    timer = lv_timer_create(lv_timer_cb, 1000 / avi->fps, NULL);
-    lv_timer_set_repeat_count(timer, -1);
-    lv_vendor_disp_unlock();
+    bk_avi_play_start();
 
     lv_vendor_start();
 
