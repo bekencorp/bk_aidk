76f1665003f575eb15887d24da5c1f5e9fef2c22
itester <itester@aclsemi.com>
2025-05-23 21:39:31 +0800

    [CI] [59068,58924]  <multimedia> <feat> Add the eqDL loading function for audTool.

--- projects/ai_dashboard/main/audio_para.c
+++ projects/ai_dashboard/main/audio_para.c
@@ -8,34 +8,59 @@
 #if (CONFIG_HARDWARE_SPEAKER_VER == 1)
 
 #define EQ0 1
-#define EQ0A0 -1668050
-#define EQ0A1 734106
-#define EQ0B0 934084
-#define EQ0B1 -1715174
-#define EQ0B2 801474
+#define EQ0A0   -1668050
+#define EQ0A1    734106
+#define EQ0B0    934084
+#define EQ0B1   -1715174
+#define EQ0B2    801474
+#define EQ0FREQ  0x44160000
+#define EQ0GAIN  0xc1700000
+#define EQ0QVAL  0x3f800000
+#define EQ0FTYPE 0x01
 
 #define EQ1 1
-#define EQ1A0 -1764715
-#define EQ1A1 784980
-#define EQ1B0 1034243
-#define EQ1B1 -1764715
-#define EQ1B2 799312
+#define EQ1A0    -1764715
+#define EQ1A1     784980
+#define EQ1B0     1034243
+#define EQ1B1    -1764715
+#define EQ1B2     799312
+#define EQ1FREQ   0x442f0000
+#define EQ1GAIN   0xbf800000
+#define EQ1QVAL   0x3f800000
+#define EQ1FTYPE  0x0
+
+#define EQSAMP   0x3e80
+#define EQGAIN   0x4000
+#define EQFGAIN  0x00000000
+
 
 #else
 
 #define EQ0 1
-#define EQ0A0 -1668050
-#define EQ0A1 734106
-#define EQ0B0 934084
-#define EQ0B1 -1715174
-#define EQ0B2 801474
+#define EQ0A0   -1668050
+#define EQ0A1    734106
+#define EQ0B0    934084
+#define EQ0B1   -1715174
+#define EQ0B2    801474
+#define EQ0FREQ  0x44160000
+#define EQ0GAIN  0xc1700000
+#define EQ0QVAL  0x3f800000
+#define EQ0FTYPE 0x01
 
 #define EQ1 1
-#define EQ1A0 -1764715
-#define EQ1A1 784980
-#define EQ1B0 1034243
-#define EQ1B1 -1764715
-#define EQ1B2 799312
+#define EQ1A0    -1764715
+#define EQ1A1     784980
+#define EQ1B0     1034243
+#define EQ1B1    -1764715
+#define EQ1B2     799312
+#define EQ1FREQ   0x442f0000
+#define EQ1GAIN   0xbf800000
+#define EQ1QVAL   0x3f800000
+#define EQ1FTYPE  0x0
+
+#define EQSAMP   0x3e80
+#define EQGAIN   0x4000
+#define EQFGAIN  0x00000000
 
 
 #endif
@@ -44,8 +69,8 @@
 
 #define CUST_EQ_PARA_DL_VOICE()                   \
     {                                        \
-    .eq_en = 1,                                                              \
-    .filters = 2,                                                             \
+    .eq_en = 1,                                                                 \
+    .filters = 2,                                                               \
     .globle_gain = (uint32_t)(1.12f * (1 << FILTER_PREGAIN_FRA_BITS)),          \
     .eq_para[0].a[0] = -EQ0A0,                                                  \
     .eq_para[0].a[1] = -EQ0A1,                                                  \
@@ -57,12 +82,24 @@
     .eq_para[1].b[0] = EQ1B0,                                                   \
     .eq_para[1].b[1] = EQ1B1,                                                   \
     .eq_para[1].b[2] = EQ1B2,                                                   \
+    .eq_load.f_gain     = EQFGAIN,                                                   \
+    .eq_load.samplerate = EQSAMP,                                                    \
+    .eq_load.eq_load_para[0].freq   = EQ0FREQ,                                               \
+    .eq_load.eq_load_para[0].gain   = EQ0GAIN,                                               \
+    .eq_load.eq_load_para[0].q_val  = EQ0QVAL,                                               \
+    .eq_load.eq_load_para[0].type   = EQ0FTYPE,                                              \
+    .eq_load.eq_load_para[0].enable  = EQ0,                                                  \
+    .eq_load.eq_load_para[1].freq   = EQ1FREQ,                                               \
+    .eq_load.eq_load_para[1].gain   = EQ1GAIN,                                               \
+    .eq_load.eq_load_para[1].q_val  = EQ1QVAL,                                               \
+    .eq_load.eq_load_para[1].type   = EQ1FTYPE,                                              \
+    .eq_load.eq_load_para[1].enable  = EQ1,                                                  \
 }
 
 #define CUST_EQ_PARA_UL_VOICE()                                              \
-    {                                                                        \
-    .eq_en = 1,                                                              \
-    .filters = 2,                                                             \
+    {                                        \
+    .eq_en = 1,                                                                 \
+    .filters = 2,                                                               \
     .globle_gain = (uint32_t)(1.12f * (1 << FILTER_PREGAIN_FRA_BITS)),          \
     .eq_para[0].a[0] = -EQ0A0,                                                  \
     .eq_para[0].a[1] = -EQ0A1,                                                  \
